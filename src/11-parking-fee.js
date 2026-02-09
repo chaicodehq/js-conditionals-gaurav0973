35cd08825ca875b3511c622392bee33bb92afb6f
gaurav0973 <codejourneypanda@gmail.com>
2026-02-09 21:45:01 +0530

    Completed the assignment

--- src/11-parking-fee.js
+++ src/11-parking-fee.js
@@ -33,5 +33,33 @@
  * @returns {number} Parking fee or -1 for invalid input
  */
 export function calculateParkingFee(hours, vehicleType) {
-  // Your code here
+  if (hours <= 0) return -1;
+  const rates = {
+    car: { 
+      first: 5,
+      additional: 3, 
+      max: 30 },
+    motorcycle:{ 
+      first: 3, 
+      additional: 2, 
+      max: 18 
+    },
+    bus: { 
+      first: 10, 
+      additional: 7,
+      max: 60 
+    },
+  };
+
+
+  if (!rates[vehicleType]) 
+    return -1;
+
+  const { first, additional, max } = rates[vehicleType];
+  const totalHours = Math.ceil(hours);
+  let fee = first;
+  if (totalHours > 1) {
+    fee += (totalHours - 1) * additional;
+  }
+  return Math.min(fee, max);
 }
